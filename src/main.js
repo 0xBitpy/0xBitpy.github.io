6956a0a9b0b3f0c896fc0ba98e94ead986fa9c14
0xBitpy <szafeczkarr77@gmail.com>
2025-11-11 12:03:08 +0100

    links update

--- src/main.js
+++
@@ -1,5 +0,0 @@
-function imageLoad(source, desc, update){
-	document.getElementById('imageload').src = source;
-	document.getElementById('textload').innerHTML = desc;
-	document.getElementById('lastupdate').innerHTML = update;
-}
